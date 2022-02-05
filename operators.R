08835684b33b7c51da2afb2b008d905caa64aed8
luaroncrew <ma-kir.ru@yandex.ru>
2022-02-05 17:23:58 +0100

    functions finished

---
+++ operators.R
@@ -0,0 +1,14 @@
+# logic operators
+a = c(1, 2, 3, 9)
+b = c(1, 1, 6, 9)
+
+a > b
+(a > 4) & (a > b)
+(a == 1) | (b > 1)
+
+
+# comparing to None
+breakfast = c("pizza", "burger", NA, "Sushi")
+is.na(breakfast)
+
+
