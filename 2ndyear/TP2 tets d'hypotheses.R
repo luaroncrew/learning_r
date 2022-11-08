f861036168463d97bc1b9beef2f3acbcb9d46f6e
luaroncrew <ma-kir.ru@yandex.ru>
2022-11-08 15:51:18 +0100

    tp

--- 2ndyear/TP2 tets d'hypotheses.R
+++ 2ndyear/TP2 tets d'hypotheses.R
@@ -11,3 +11,33 @@ test$expected
 
 # p-value is low and lower than 5% so we reject H0
 
+# ex 2: test of homogenity
+
+populations = read.xlsx('data/practical 2.xlsx', 2, rowNames=TRUE, colNames=TRUE)
+
+populations
+test = chisq.test(populations$Sample.1, populations$Sample.2)
+test
+
+# p-value is 23.9% so we accept the H0: the populations in the sample and 
+# the real one are the same
+
+# H0 theoretical and the sample's distributions
+# ex 2: 
+freq = populations$Population/sum(populations$Population)
+
+test = chisq.test(populations$Sample.1, p=freq)
+
+test$p.value
+
+# p-value is 0.004 so we reject the H0
+
+
+# EX3 
+populations = read.xlsx('data/practical 2.xlsx', 3,)
+
+chisq.test(populations[,1:2])
+
+
+
+
